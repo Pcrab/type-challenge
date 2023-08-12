8394d20fea1dba6c26e003195d29dc0d64308e9f
pcrab <pcrab@pcrab.xyz>
2023-08-12 15:07:07 +0800

    feat: 2793-mutable

---
+++ src/medium/2793-mutable.ts
@@ -0,0 +1,63 @@
+/*
+  2793 - Mutable
+  -------
+  by jiangshan (@jiangshanmeta) #medium #readonly #object-keys
+
+  ### Question
+
+  Implement the generic ```Mutable<T>``` which makes all properties in ```T``` mutable (not readonly).
+
+  For example
+
+  ```typescript
+  interface Todo {
+    readonly title: string
+    readonly description: string
+    readonly completed: boolean
+  }
+
+  type MutableTodo = Mutable<Todo> // { title: string; description: string; completed: boolean; }
+
+  ```
+
+  > View on GitHub: https://tsch.js.org/2793
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Mutable<T extends object> = {
+    -readonly [key in keyof T]: T[key];
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Todo1 {
+    title: string;
+    description: string;
+    completed: boolean;
+    meta: {
+        author: string;
+    };
+}
+
+type List = [1, 2, 3];
+
+export type cases = [
+    Expect<Equal<Mutable<Readonly<Todo1>>, Todo1>>,
+    Expect<Equal<Mutable<Readonly<List>>, List>>,
+];
+
+export type errors = [
+    // @ts-expect-error "string" is not an object
+    Mutable<"string">,
+    // @ts-expect-error 0 is not an object
+    Mutable<0>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2793/answer
+  > View solutions: https://tsch.js.org/2793/solutions
+  > More Challenges: https://tsch.js.org
+*/
