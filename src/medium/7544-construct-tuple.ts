367646b080d920eecd12131dbf4a9a57ba91834a
pcrab <pcrab@pcrab.xyz>
2023-08-13 16:03:27 +0800

    feat: 7544-construct-tuple

---
+++ src/medium/7544-construct-tuple.ts
@@ -0,0 +1,44 @@
+/*
+  7544 - Construct Tuple
+  -------
+  by Lo (@LoTwT) #medium #tuple
+
+  ### Question
+
+  Construct a tuple with a given length.
+
+  For example
+
+  ```ts
+  type result = ConstructTuple<2> // expect to be [unknown, unkonwn]
+  ```
+
+  > View on GitHub: https://tsch.js.org/7544
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type ConstructTuple<
+    L extends number,
+    Current extends unknown[] = [],
+> = Current["length"] extends L
+    ? Current
+    : ConstructTuple<L, [...Current, unknown]>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+export type cases = [
+    Expect<Equal<ConstructTuple<0>, []>>,
+    Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
+    Expect<Equal<ConstructTuple<999>["length"], 999>>,
+    // @ts-expect-error too deep
+    Expect<Equal<ConstructTuple<1000>["length"], 1000>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/7544/answer
+  > View solutions: https://tsch.js.org/7544/solutions
+  > More Challenges: https://tsch.js.org
+*/
